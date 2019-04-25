ccccd0c14563ea70816906634c9d5ad044f1e1f4
dongdangsss <dongdang@success-ss.com.vn>
2019-04-25 16:42:10 +0700

    import mongo and connect

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+
+const user = encodeURIComponent('dongadmin');
+const password = encodeURIComponent('0989886285Aa');
+const dbName = 'test'
+
+const uri = `mongodb+srv://${user}:<${password}>@dong-dlkhv.mongodb.net/${dbName}?retryWrites=true`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true
+});
+
+client.connect(err => {
+  if(err) {
+    console.error(err)
+  };
+
+  if(client.isConnected()) {
+    const db = client.db(dbName);
+  }
+  // perform actions on the collection object
+  console.log("Connected successfully to server");
+
+  client.close();
+});
